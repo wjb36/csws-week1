5fb0fdac0be0ad795a3f2a15b990401c8c535563
Bill Byrne <billbyrne1@outlook.com>
2023-01-27 13:02:56 +0000

    reverse and sort

---
+++ week2.py
@@ -0,0 +1,13 @@
+friends = ["Josh", "Elliot", "Maaria", "Ellie"]
+print(friends)
+friends.reverse()
+print(friends)
+friends.sort()
+print(friends)
+friends.reverse()
+print(friends)
+
+
+
+for x in range(10):
+    print("Hello World!" + str(x))
