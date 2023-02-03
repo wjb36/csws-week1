8f2960002e70414bbbfa2eda1947f5f09e1ff360
Bill Byrne <billbyrne1@outlook.com>
2023-02-03 12:26:49 +0000

    sum cubes

--- week3t1.py
+++ week3t1.py
@@ -1,3 +1,13 @@
 num_list = range(1, 1000001)
-for x in range(1, 1000001):
-    print(num_list[x])
+#for x in range(1, 1000001):
+#    print(num_list[x])
+
+print(f"The sum of the numbers from 1 to 1000000 is {sum(num_list)}")
+
+number_43 = range(1, 11)
+
+
+for x in range(1, 11):
+    cubes = [x**3 for x in range(1, 11)]
+print(f"The sum of cubes from 1 to 10 is {sum(cubes)}")
+
